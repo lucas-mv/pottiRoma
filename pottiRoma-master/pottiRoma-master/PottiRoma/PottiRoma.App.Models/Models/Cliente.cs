0e05f54d482a72c9802cdea2838978577f7bc87c
LUCAS ROSCOE <lucas.roscoe@dtidigital.com.br>
2018-01-07 14:58:33 -0200

    commit

---
+++ pottiRoma-master/pottiRoma-master/PottiRoma/PottiRoma.App.Models/Models/Cliente.cs
@@ -0,0 +1,51 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PottiRoma.App.Models.Models
+{
+    public class Cliente : BindableBase
+    {
+        private string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+            set { SetProperty(ref _nome, value); }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { SetProperty(ref _email, value); }
+        }
+
+        private string _telefone;
+        public string Telefone
+        {
+            get { return _telefone; }
+            set { SetProperty(ref _telefone, value); }
+        }
+
+        private string _endereco;
+        public string Endereco
+        {
+            get { return _endereco; }
+            set { SetProperty(ref _endereco, value); }
+        }
+
+        private string _cpf;
+        public string CPF
+        {
+            get { return _cpf; }
+            set { SetProperty(ref _cpf, value); }
+        }
+
+        public Guid ClienteId { get; set; }
+
+        public Guid VendedorId { get; set; }
+    }
+}
--- pottiRoma-master/pottiRoma-master/PottiRoma/PottiRoma.App/PottiRoma.App.Android/Renderers/CustomEditorAndroid.cs
+++ pottiRoma-master/pottiRoma-master/PottiRoma/PottiRoma.App/PottiRoma.App.Android/Renderers/CustomEditorAndroid.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using PottiRoma.App.CustomRenderers;
+﻿using PottiRoma.App.CustomRenderers;
 using PottiRoma.App.Droid.Renderers;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
