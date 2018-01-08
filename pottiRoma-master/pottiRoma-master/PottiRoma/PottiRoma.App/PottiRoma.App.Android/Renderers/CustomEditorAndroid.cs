a033b2450d764c832b90d3fd8e79f4c9f3ad0497
LUCAS ROSCOE <lucasrloliveira@gmail.com>
2018-01-07 23:44:16 -0200

    Revert "commit"
    This reverts commit 0e05f54d482a72c9802cdea2838978577f7bc87c.

--- pottiRoma-master/pottiRoma-master/PottiRoma/PottiRoma.App/PottiRoma.App.Android/Renderers/CustomEditorAndroid.cs
+++ pottiRoma-master/pottiRoma-master/PottiRoma/PottiRoma.App/PottiRoma.App.Android/Renderers/CustomEditorAndroid.cs
@@ -1,4 +1,15 @@
-﻿using PottiRoma.App.CustomRenderers;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using PottiRoma.App.CustomRenderers;
 using PottiRoma.App.Droid.Renderers;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
