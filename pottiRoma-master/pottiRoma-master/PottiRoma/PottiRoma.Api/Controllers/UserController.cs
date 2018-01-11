29bfe28f056458cdfafee991742dc2ecad575866
LUCAS ROSCOE <lucasrloliveira@gmail.com>
2018-01-11 17:44:44 -0200

    Servico de envio de email em andamento

--- pottiRoma-master/pottiRoma-master/PottiRoma/PottiRoma.Api/Controllers/UserController.cs
+++ pottiRoma-master/pottiRoma-master/PottiRoma/PottiRoma.Api/Controllers/UserController.cs
@@ -1,10 +1,14 @@
-﻿using PottiRoma.Api.Request.User;
+﻿using PottiRoma.Api.Helpers;
+using PottiRoma.Api.Request.Email;
+using PottiRoma.Api.Request.User;
+using PottiRoma.Api.Response.Email;
 using PottiRoma.Api.Response.User;
 using PottiRoma.Services.Interfaces;
 using PottiRoma.Utils.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 
@@ -39,5 +43,13 @@ namespace PottiRoma.Api.Controllers
                 User = _userService.RegisterUser(request.Email, request.Password, request.Name, request.PrimaryTelephone, request.SecondaryTelephone, request.Cpf, request.UserType)
             };
         }
+
+        //[HttpPost]
+        //[Route("EnviarEmail")]
+        //public async Task<SendEmailResponse> EnviarEmail(SendEmailRequest request)
+        //{
+        //    //Email.Enviar(request.Assunto, request.CorpoEmail, request.Destinatarios, request.Cc, null, , emailNomeFrom.Item2, _logoPath);
+        //    //return ResponseResult(string.Empty);
+        //}
     }
 }
