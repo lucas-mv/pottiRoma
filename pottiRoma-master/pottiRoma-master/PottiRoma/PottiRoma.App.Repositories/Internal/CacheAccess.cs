481c9c9c5c4ab59791ea83941a5085277bf5b0d6
Lucas Moura Veloso <lmouraveloso@gmail.com>
2018-01-12 17:29:40 -0200

    troca de cache em memoria por cache local

--- pottiRoma-master/pottiRoma-master/PottiRoma/PottiRoma.App.Repositories/Internal/CacheAccess.cs
+++ pottiRoma-master/pottiRoma-master/PottiRoma/PottiRoma.App.Repositories/Internal/CacheAccess.cs
@@ -18,18 +18,18 @@ namespace PottiRoma.App.Repositories.Internal
 
         public async static Task DeleteAll()
         {
-            await BlobCache.InMemory.InvalidateAll();
+            await BlobCache.LocalMachine.InvalidateAll();
             await BlobCache.Secure.InvalidateAll();
         }
 
         public async static Task Insert<T>(string key, T blob)
         {
-            await BlobCache.InMemory.InsertObject<T>(key, blob);
+            await BlobCache.LocalMachine.InsertObject<T>(key, blob);
         }
 
         public async static Task<T> Get<T>(string key)
         {
-            return await BlobCache.InMemory.GetObject<T>(key);
+            return await BlobCache.LocalMachine.GetObject<T>(key);
         }
 
         public async static Task InsertSecure<T>(string key, T blob)
