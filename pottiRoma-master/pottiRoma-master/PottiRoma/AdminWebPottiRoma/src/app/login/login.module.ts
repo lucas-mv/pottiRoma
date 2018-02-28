a1cd88b0af76b1553a2dac3059d81d779d57b296
LUCAS ROSCOE <lucasrloliveira@gmail.com>
2018-02-28 10:03:24 -0300

    commit

--- pottiRoma-master/pottiRoma-master/PottiRoma/AdminWebPottiRoma/src/app/login/login.module.ts
+++ pottiRoma-master/pottiRoma-master/PottiRoma/AdminWebPottiRoma/src/app/login/login.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
 @NgModule({
-    imports: [CommonModule, LoginRoutingModule],
+    imports: [CommonModule, LoginRoutingModule, MatButtonModule, MatCheckboxModule],
+    exports: [MatButtonModule, MatCheckboxModule],
     declarations: [LoginComponent]
 })
 export class LoginModule {}
