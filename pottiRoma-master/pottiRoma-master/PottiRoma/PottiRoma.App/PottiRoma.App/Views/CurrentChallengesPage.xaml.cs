7fc749e45993236dfbe3f90a762e8d5e83818131
LUCAS ROSCOE <lucasrloliveira@gmail.com>
2018-03-10 15:12:18 -0300

    App praticamente finalizado

--- pottiRoma-master/pottiRoma-master/PottiRoma/PottiRoma.App/PottiRoma.App/Views/CurrentChallengesPage.xaml.cs
+++ pottiRoma-master/pottiRoma-master/PottiRoma/PottiRoma.App/PottiRoma.App/Views/CurrentChallengesPage.xaml.cs
@@ -7,6 +7,7 @@ namespace PottiRoma.App.Views
         public CurrentChallengesPage()
         {
             InitializeComponent();
+            NavigationPage.SetBackButtonTitle(this, "Voltar");
         }
     }
 }
--- pottiRoma-master/pottiRoma-master/PottiRoma/PottiRoma.App/PottiRoma.App/Views/InviteFlowerPage.xaml.cs
+++ pottiRoma-master/pottiRoma-master/PottiRoma/PottiRoma.App/PottiRoma.App/Views/InviteFlowerPage.xaml.cs
@@ -7,6 +7,7 @@ namespace PottiRoma.App.Views
         public InviteFlowerPage()
         {
             InitializeComponent();
+            NavigationPage.SetBackButtonTitle(this, "Voltar");
         }
     }
 }
